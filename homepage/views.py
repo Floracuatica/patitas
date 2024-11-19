eb2195f87eb2f1b7fb5f5c30d791d636351066f2
Floracuatica <rsbejar@gmail.com>
2024-11-19 20:13:43 -0300

    fixing bootstrap

--- homepage/views.py
+++ homepage/views.py
@@ -3,6 +3,28 @@ from django.shortcuts import render
 
 # Create your views here.
 def v_index(request):
-    context = {}
+    context = {
+        "providers": [
+            {
+                "first_name": "Pepito",
+                "last_name": "Rojas"
+            },
+            {
+                "first_name": "Felipe",
+                "last_name": "Iero"
+            }, 
+            {   "first_name": "Amanda",
+                "last_name": "Miguel"
+            }, 
+            {   "first_name": "Carlos",
+                "last_name": "Mueres"
+            }, 
+            {   "first_name": "Pablo",
+                "last_name": "Horrores"
+            }, 
+            {   "first_name": "Felicita",
+                "last_name": "Bonita"}
+        ]
+    }
     return render(request, "homepage/index.html", 
                   context)

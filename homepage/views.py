725d5c6417f54c01f34c6cfc9ed88ef78be675d9
Floracuatica <rsbejar@gmail.com>
2024-11-22 20:21:13 -0300

    gitignore staticfiles

--- homepage/views.py
+++ homepage/views.py
@@ -6,24 +6,32 @@ def v_index(request):
     context = {
         "providers": [
             {
-                "first_name": "Pepito",
-                "last_name": "Rojas"
+                "first_name": "Thomas",
+                "last_name": "Reed",
+                "photo": "https://images-assets.nasa.gov/image/iss072e145396/iss072e145396~medium.jpg"
             },
             {
-                "first_name": "Felipe",
-                "last_name": "Iero"
+                "first_name": "Mark",
+                "last_name": "Iero",
+                "photo": "https://images-assets.nasa.gov/image/iss072e145408/iss072e145408~medium.jpg"
             }, 
-            {   "first_name": "Amanda",
-                "last_name": "Miguel"
+            {   "first_name": "Andy",
+                "last_name": "Warhol",
+                "photo": "https://images-assets.nasa.gov/image/iss072e188539/iss072e188539~medium.jpg"
             }, 
-            {   "first_name": "Carlos",
-                "last_name": "Mueres"
+            {   "first_name": "Holden",
+                "last_name": "Dix",
+                "photo": "https://images-assets.nasa.gov/image/iss072e189176/iss072e189176~medium.jpg"
             }, 
             {   "first_name": "Pablo",
-                "last_name": "Horrores"
+                "last_name": "Honey",
+                "photo": "https://images-assets.nasa.gov/image/KSC-20241107-PH-KLS01_0011/KSC-20241107-PH-KLS01_0011~medium.jpg"
             }, 
-            {   "first_name": "Felicita",
-                "last_name": "Bonita"}
+            {   "first_name": "Phillip",
+                "last_name": "Bells",
+                "photo": "https://images-assets.nasa.gov/image/JPL-20170821-SUNs-000C-Eclipse3/JPL-20170821-SUNs-000C-Eclipse3~medium.jpg"
+            }
+                
         ]
     }
     return render(request, "homepage/index.html", 
